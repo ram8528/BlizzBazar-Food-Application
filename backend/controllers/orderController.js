98e64bcbb3670518010305b56528dfe7208cb7d4
ram8528 <106538883+ram8528@users.noreply.github.com>
2025-08-26 00:07:17 +0530

    feat: Implement order verification and user orders functionality with corresponding routes and UI updates

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,6 +1,6 @@
-import orderModel from "../models/orderModel";
-import userModel from "../models/userModel";
-import stripe, { Stripe } from "stripe";
+import orderModel from "../models/orderModel.js";
+import userModel from "../models/userModel.js";
+import { Stripe } from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
@@ -10,10 +10,10 @@ const placeOrder = async (req, res) => {
   const frontend_url = "http://localhost:5173";
   try {
     const newOrder = new orderModel({
-      userId: req.body.userId,
+      userId: req.userId,
       items: req.body.items,
       amount: req.body.amount,
-      address: req.nody.address,
+      address: req.body.address,
     });
     await newOrder.save();
     await userModel.findByIdAndUpdate(req.body.userId, { cartData: {} });
@@ -52,9 +52,36 @@ const placeOrder = async (req, res) => {
     console.log(error);
     res.json({
       success: false,
-      message: "Error",
+      message: "Error in backend",
     });
   }
 };
 
-export { placeOrder };
+const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  try {
+    if (success == "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Not paid" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+// user orders for frontend
+const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.userId });
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders };
